2c76b0e675dfe65398f711e7ce72ec6cde2491d5
Viacheslav Minakov <w.mnkv@Viacheslavs-MacBook-Pro.local>
2024-07-10 06:20:44 +0300

    homework2

---
+++ HW_02/2.js
@@ -0,0 +1,12 @@
+/*Задание 2
+Перепишите код к тернарному оператору
+
+let test = true;
+if (test === true) {
+console.log('+++');
+} else {
+console.log('---');
+}*/
+
+let test = true;
+console.log((test)? '+++' : '---');
